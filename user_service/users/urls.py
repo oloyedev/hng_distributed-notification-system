47153a44bb48fa43e4aa0b1b65cc8add03d93f84
michaelEmeka <mikelonu15@gmail.com>
2025-11-10 16:36:04 +0100

    feat(user_service): add login functionality

--- user_service/users/urls.py
+++ user_service/users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.CreateUserView.as_view(), name="create_user"),
+    path("login", views.LoginUserView.as_view(), name="login_user"),
 ]
--- user_service/users/views.py
+++ user_service/users/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import CreateUserSerializer
-
+from .models import User
 
 class CreateUserView(APIView):
     """
@@ -27,3 +27,29 @@ class CreateUserView(APIView):
             return Response({"message": f"User {user.email} created successfully"}, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginUserView(APIView):
+    """
+    Request Body:{
+      email: Email
+      password: str
+    }
+    """
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                tokens = user.tokens()
+                return Response(
+                    {
+                        "access_tokens": tokens["access"],
+                        "refresh_token": tokens["refresh"]
+                    }, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+        except User.DoesNotExist:
+            return Response({"error": "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
